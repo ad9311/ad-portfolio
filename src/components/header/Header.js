a08b00aca6acce07f55941babc3c277afda4fe39
Ángel Díaz <diazangel9311@gmail.com>
2022-01-13 11:22:27 -0500

    WIP fix useEffect dependency issue

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
@@ -27,7 +28,7 @@ const Header = () => {
         dispatch(selectProject('default'));
       }
     }
-  }, [target, dispatch, selected, show]);
+  }, [target]);
 
   return (
     <header className={
